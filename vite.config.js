43f35340652fbe08a93daba674c92560edf53f25
gonzalo mina <gonzalo.mina@hotmail.com.ar>
2023-01-23 21:22:24 -0300

    first comit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/lista-tareas-react/",
+  base: "/lista-de-tareas-ahora-si-esto-es-personal/",
 })
