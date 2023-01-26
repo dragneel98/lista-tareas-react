4eed8c75da80cac361c4bc8b5d469fd9365dccb7
gonzalo mina <gonzalo.mina@hotmail.com.ar>
2023-01-25 21:30:12 -0300

    corregi la url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/https://dragneel98.github.io/lista-tareas-react/",
+  base: "https://dragneel98.github.io/lista-tareas-react/",
 })
